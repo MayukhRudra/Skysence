6026d03d926fe406f86c172ed9a72246f27eb83b
MayukhRudra <mayukhrudra52@gmail.com>
2025-11-01 17:19:00 +0530

    f

--- app.py
+++ app.py
@@ -30,17 +30,26 @@ def weather_by_coords():
 @app.route('/', methods=['GET', 'POST'])
 def index():
     data = None
+    error_message = None
     city = state = country = ""
+    
     if request.method == 'POST':
         city = request.form['cityName']
         state = request.form['stateName']
         country = request.form['countryName']
         data = get_weather(city, state, country)
+        
+        # Check if there's an error in the response
+        if isinstance(data, dict) and 'error' in data:
+            error_message = data['error']
+            data = None
+        
         session.pop('auto_data', None)
     elif 'auto_data' in session:
         data = session['auto_data']
        
-    return render_template('index.html', data=data, city=city, state=state, country=country)
+    return render_template('index.html', data=data, error=error_message, 
+                         city=city, state=state, country=country)
 
 if __name__ == '__main__':
     app.run(debug=True)
